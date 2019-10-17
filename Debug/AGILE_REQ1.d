b0437aa0e76d1868484b0aba24c842b5907c7056
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-10-18 00:47:13 +0200

    Create AGILE_REQ1.d

---
+++ Debug/AGILE_REQ1.d
@@ -0,0 +1,23 @@
+AGILE_REQ1.o AGILE_REQ1.o: ../AGILE_REQ1.c ../DIO.h ../registers.h \
+  ../stdtypes.h ../DIO_cnfg.h ../UART.h ../LCD.h ../INTERFACE.h \
+  ../UNIT_types.h ../MICRO_setting.h ../keypad.h
+
+../DIO.h:
+
+../registers.h:
+
+../stdtypes.h:
+
+../DIO_cnfg.h:
+
+../UART.h:
+
+../LCD.h:
+
+../INTERFACE.h:
+
+../UNIT_types.h:
+
+../MICRO_setting.h:
+
+../keypad.h:
